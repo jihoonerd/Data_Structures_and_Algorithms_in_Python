5e0f52ce80f18e25a4cb7843c59ed5ae15f442f8
Jihoon Kim <jihoonerd@outlook.com>
2017-06-15 11:25:19 +0900

    Stack w/ Python array

---
+++ Concepts/Chapter06/ArrayStack.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 09:38:13 2017
+
+@author: Jihoon_Kim
+"""
+
+
+# Stack
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+
+class ArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+
+    def __init__(self):
+        """Create an empty stack."""
+        self._data = []
+
+    def __len__(self):
+        """Return the number of elements in the stack"""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return len(self._data) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        self._data.append(e)
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack.
+
+        Raise Empty exception if the stack is empty.
+        """
+
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e.,LIFO)
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
