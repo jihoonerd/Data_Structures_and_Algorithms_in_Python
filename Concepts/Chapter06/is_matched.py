464036c1e3e905c0f08e1136c8354fb038b174a0
Jihoon Kim <jihoonerd@outlook.com>
2017-06-15 11:42:26 +0900

    is_matched: html supported

--- Concepts/Chapter06/is_matched.py
+++ Concepts/Chapter06/is_matched.py
@@ -24,3 +24,24 @@ def is_matched(expr):
             if righty.index(c) != lefty.index(S.pop()):
                 return False
     return S.is_empty()
+
+
+def is_matched_html(raw):
+    """Return True if all HTML tags are properly match; False otherwise."""
+    S = ArrayStack()
+    j = raw.find('<')
+
+    while j != -1:
+        k = raw.find('>', j+1)
+        if k == -1:
+            return False
+        tag = raw[j+1:k]
+        if not tag.startswith('/'):
+            S.push(tag)
+        else:
+            if S.is_empty():
+                return False
+            if tag[1:] != S.pop():
+                return False
+        j = raw.find('<', k+1)
+    return S.is_empty()
